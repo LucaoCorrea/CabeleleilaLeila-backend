faf26dbc496a9cb58c327a5a10735ca8a1f3bb5f
LucaoCorrea <lucascorrea1505@gmail.com>
2025-03-14 11:24:16 -0300

    Create Project

---
+++ leilaSalao/src/main/java/com/leila/leilaSalao/LeilaSalaoApplication.java
@@ -0,0 +1,13 @@
+package com.leila.leilaSalao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeilaSalaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeilaSalaoApplication.class, args);
+	}
+
+}
---
+++ leilaSalao/src/test/java/com/leila/leilaSalao/LeilaSalaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leila.leilaSalao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeilaSalaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
