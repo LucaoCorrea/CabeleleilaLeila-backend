bca44b13133b6b5b1495fd291e5c1ca09d2e7f6b
LucaoCorrea <lucascorrea1505@gmail.com>
2025-03-15 20:31:35 -0300

    .

--- leilaSalao/src/main/java/com/leila/leilaSalao/appointment/Appointment.java
+++ leilaSalao/src/main/java/com/leila/leilaSalao/appointment/Appointment.java
@@ -3,10 +3,20 @@ package com.leila.leilaSalao.appointment;
 import java.time.LocalDateTime;
 
 import com.leila.leilaSalao.model.User;
-import jakarta.persistence.*;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
 @Setter
